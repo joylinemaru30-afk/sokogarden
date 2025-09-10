16b2d0690e9b198da68545ac91e7eeaca7944295
pi342us-g <ndubipius96@gmail.com>
2025-09-10 12:25:35 +0300

    task

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Signin = () => {
+  const[email,setemail]=useState("")
+  const[password,setpassword]=useState("")
+
   return (
-    <div>
-        <h1>welcome to the signin page</h1>
+    <div className="row justify-content-center mt-4">
+      <div className="col-md-6 p-4 card shadow">
+        <h1>signin</h1>
+        <form >
+          <input type="text"
+          className="form-control"
+          placeholder="enter your email here"
+          value={email}
+          onChange={(e)=>setemail(e.target.value)}
+          
+          />
+          {email}
+          <br />
+          <input type="text" 
+          className="form-control"
+          placeholder="enter your password here"
+          value={password}
+          onChange={(e)=>setpassword(e.target.value)}
+          
+          />
+          {password}
+          <br />
+          <button type="submit" className="btn btn-success">signup</button>
+        
+        </form>
         
     </div>
+    </div>
+
   )
 }
 
